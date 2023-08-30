361ddbcdce41bd2baa29f13123997e51620f849c
Ian Daniel <66013003+IanDanielM@users.noreply.github.com>
2023-08-30 15:21:12 +0100

    user API

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,6 @@
 """views for the user API"""
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -17,3 +17,15 @@ class CreataTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """retrieve and return the authenticated user"""
+        return self.request.user
