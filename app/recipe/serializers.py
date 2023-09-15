ade13be0be99c9f01f52f232731ce696bd6b9a0e
Ian Daniel <66013003+IanDanielM@users.noreply.github.com>
2023-09-15 20:08:03 +0100

    Image & Filter Api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -83,4 +83,14 @@ class RecipeDetailSerializer(RecipeSerializer):
     """serializer for Recipe Detail View"""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """serializer for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
